7319373c5f277b3a50e0f0ee7fd0f399894f0400
Daniel Tao <danieltaox@gmail.com>
2019-03-19 23:37:24 -0400

    Bootstrap Django app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <h1>MEMES</h1>;
+  }
+}
+
+export default App;
---
+++ frontend/src/Image.js
@@ -0,0 +1,12 @@
+class Image extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render() {
+
+    }
+}
